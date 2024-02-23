fca440e62c92fc01a1fcae4ff23403d6bbdba677
Thomasson <turodoras@gmail.com>
2024-02-23 17:27:13 +0100

    Add excel array to store amp & spk doc

--- main.py
+++ main.py
@@ -1,49 +1,59 @@
-from math import *
+from pathlib import Path
+from pprint import pprint
+from pandas import read_excel
+from math import log10, sqrt
 from decimal import Decimal, ROUND_DOWN, ROUND_UP
 
 
+INVENTORY_NAME = r"inventory.xlsx"
+INVENTORY_PATH = str(Path(__file__).parent.resolve()) + "\\" + INVENTORY_NAME
+
+
 class Speaker:
-    def __init__(self, _name: str, _power: int, _impedance: int, _baffle: int) -> None:
+    def __init__(self, _name: str, _reference: str, _impedance: int, _power: int, _response: str, _baffle: str) -> None:
         self.name = _name
-        self.power = _power
+        self.reference = _reference
         self.impedance = _impedance
+        self.power = _power
+        self.response = _response
         self.baffle = _baffle
 
 class Amplifier:
-    def __init__(self, _name: str, _power: dict, _gain: int) -> None:
+    def __init__(self, _name: str, _reference: str, _gain: int, _power: dict, _outputs: int) -> None:
         self.name = _name
-        self.power = _power
+        self.reference = _reference
         self.gain = _gain
+        self.power = _power
+        self.outputs = _outputs
 
 
-SEALED = 0
-OPEN = 1
+def getSpecs(path: str) -> tuple[list[Speaker], list[Amplifier]]:
+    """Return list of speakers & amplifiers specs from inventory
+    """
 
-speakers = {
-    "MTH": Speaker(_name="MTH", _power=1500, _impedance=8, _baffle=SEALED),
-    "F221": Speaker(_name="F221", _power=2000, _impedance=4, _baffle=SEALED),
-    "MKH": Speaker(_name="MKH", _power=900, _impedance=4, _baffle=SEALED),
-    "BPH": Speaker(_name="BPH", _power=1000, _impedance=4, _baffle=SEALED),
-    "NEXOK": Speaker(_name="NEXOK", _power=600, _impedance=4, _baffle=OPEN),
-    "NEXOM": Speaker(_name="NEXOM", _power=350, _impedance=8, _baffle=SEALED),
-    "NEXOH": Speaker(_name="NEXOH", _power=150, _impedance=8, _baffle=SEALED),
-    "ERICS": Speaker(_name="ERICS", _power=1000, _impedance=4, _baffle=OPEN),
-    "ERICL": Speaker(_name="ERICL", _power=500, _impedance=8, _baffle=OPEN),
-    "ERICM": Speaker(_name="ERICM", _power=400, _impedance=8, _baffle=SEALED),
-    "ERICH": Speaker(_name="ERICH", _power=320, _impedance=8, _baffle=SEALED),
-    "ORPH": Speaker(_name="ORPH", _power=700, _impedance=8, _baffle=SEALED),
-    "MINI-ORPH": Speaker(_name="MINI-ORPH", _power=300, _impedance=8, _baffle=SEALED),
-    "TMS3L": Speaker(_name="TMS3L", _power=500, _impedance=4, _baffle=SEALED),
-    "TMS3M": Speaker(_name="TMS3M", _power=200, _impedance=8, _baffle=SEALED),
-    "TMS3H": Speaker(_name="TMS3H", _power=75, _impedance=8, _baffle=SEALED),
-}
-amplis = {
-    "TSA": Amplifier(_name="TSA", _power={"4": 1670, "8": 1220}, _gain=37.8),
-    "TPA": Amplifier(_name="TPA", _power={"4": 4400, "8": 2350}, _gain=35),
-    "9001": Amplifier(_name="9001", _power={"4": 2050, "8": 1100}, _gain=40),
-    "LA": Amplifier(_name="LA", _power={"4": 1500, "8": 1100}, _gain=32),
-    "HPA": Amplifier(_name="HPA", _power={"4": 1800, "8": 1100}, _gain=36),
-}
+    data_amp = read_excel(path, sheet_name="amplifiers").to_dict(orient="records")
+    data_spk = read_excel(path, sheet_name="speakers").to_dict(orient="records")
+
+    amplis, speakers = {}, {}
+    for amp in data_amp:
+        amplis[amp["name"]] = Amplifier(
+            _name=amp["name"],
+            _reference=amp["reference"],
+            _gain=amp["gain"],
+            _power={"8": amp["power_8ohm"], "4": amp["power_4ohm"], "2": amp["power_2ohm"]},
+            _outputs=amp["outputs"],
+        )
+    for spk in data_spk:
+        speakers[spk["name"]] = Speaker(
+            _name=spk["name"],
+            _reference=spk["reference"],
+            _impedance=spk["impedance"],
+            _power=spk["power"],
+            _response=spk["response"],
+            _baffle=spk["baffle"],
+        )
+
+    return amplis, speakers
 
 
 def limit(spk: Speaker, amp: Amplifier, impedance: int, sensitivity: float=0.775) -> float:
@@ -59,7 +69,7 @@ def limit(spk: Speaker, amp: Amplifier, impedance: int, sensitivity: float=0.775
     spk_power = spk.power * (spk.impedance / impedance)
     amp_power = amp.power[str(impedance)]
 
-    lim_spk = 20 * log10(sqrt((spk_power / (1.5625 if spk.baffle == OPEN else 2.34375)) * impedance) / sensitivity) - amp.gain
+    lim_spk = 20 * log10(sqrt((spk_power / (1.5625 if spk.baffle == "OPEN" else 2.34375)) * impedance) / sensitivity) - amp.gain
     lim_amp = 20 * log10(sqrt((amp_power / 2) * impedance) / sensitivity) - amp.gain
     lim = min(lim_spk, lim_amp)
     lim_dbU = Decimal(lim).quantize(Decimal('.1'), rounding=(ROUND_DOWN if lim > 0 else ROUND_UP))
@@ -68,6 +78,9 @@ def limit(spk: Speaker, amp: Amplifier, impedance: int, sensitivity: float=0.775
 
 
 def main():
+    # on récupère les données des amplis & enceintes
+    amplis, speakers = getSpecs(INVENTORY_PATH)
+
     # on demande quelle enceinte, quel ampli et quelle impédance
     # on initialise spk et amp en leur donnant ref + impédance
     # on envoie les deux objets à calc
